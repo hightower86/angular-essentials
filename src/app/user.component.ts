09fc5f91adb1fe072c45cecb4986426423bd46ed
Adel <htwr86@gmail.com>
2020-07-20 21:48:25 +0300

    add user component

---
+++ src/app/user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <h2>Hello!</h2>
+    <p>I'm a user component</p>
+  `,
+})
+export class UserComponent {}
